fb6eb3fcfc72ca3906ec7fb48ac1c4182417450b
Fadhil Ikhsan <fadhilikhsanta@gmail.com>
2022-04-29 10:09:46 +0700

    Initial Commit

---
+++ Block Factor Measurement/Block_Factor_MeasurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_Factor_MeasurementApp.swift
+//  Block Factor Measurement
+//
+//  Created by Fadhil Ikhsanta on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Block_Factor_MeasurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
